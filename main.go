c796886ac818084a460701a7b071f8303d5d0c38
Rach Belaid <rachid.belaid@gmail.com>
2016-01-21 13:37:10 -0800

    Bump version to 0.2.0 for next release

--- main.go
+++ main.go
@@ -13,7 +13,7 @@ import (
 //go:generate go-bindata -prefix "static/" -pkg main -o bindata.go static/index.html static/favicons/... static/build/...
 
 const (
-	Version = "0.1.4"
+	Version = "0.2.0"
 )
 
 type CronValue string
